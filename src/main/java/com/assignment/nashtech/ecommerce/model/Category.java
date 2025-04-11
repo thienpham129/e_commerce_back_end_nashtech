7eba10e42c11dbfbb276c400452f71610d495d9f
thienpham129 <thienversiopp129.it.vn@gmail.com>
2025-04-11 15:10:50 +0700

    Category Model Convention, Mapping Relationship,Add Constraint

---
+++ src/main/java/com/assignment/nashtech/ecommerce/model/Category.java
@@ -0,0 +1,77 @@
+package com.assignment.nashtech.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int categoryId;
+
+    @Size(max = 256)
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Category() {
+    }
+
+    public Category(int categoryId, String categoryName, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public @Size(max = 256) String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(@Size(max = 256) String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}

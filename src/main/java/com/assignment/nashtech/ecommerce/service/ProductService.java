b197b5bd93498883bea74a7c58310b56f7a8aa66
thienpham129 <thienversiopp129.it.vn@gmail.com>
2025-04-14 18:08:34 +0700

    fix(product) - 01: missing ProductService.java when commit

--- src/main/java/com/assignment/nashtech/ecommerce/service/ProductService.java
+++ src/main/java/com/assignment/nashtech/ecommerce/service/ProductService.java
@@ -1,4 +1,20 @@
 package com.assignment.nashtech.ecommerce.service;
 
-public class ProductService {
+import com.assignment.nashtech.ecommerce.dto.ProductDTO;
+import com.assignment.nashtech.ecommerce.model.Product;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+
+    Optional<Product> getProductById(@PathVariable int productId);
+
+    Product saveProduct(ProductDTO productDTO);
+
+    Product updateProductById(int productId, ProductDTO productDTO);
+
+    void deleteProductById(int productId);
 }

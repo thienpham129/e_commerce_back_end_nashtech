42435de87b36f1d7a81e8287ea26f5fcca306685
thienpham129 <thienversiopp129.it.vn@gmail.com>
2025-04-14 16:25:40 +0700

    feat(category): CRUD Category

--- src/main/java/com/assignment/nashtech/ecommerce/service/CategoryService.java
+++ src/main/java/com/assignment/nashtech/ecommerce/service/CategoryService.java
@@ -1,4 +1,17 @@
 package com.assignment.nashtech.ecommerce.service;
 
-public class CategoryService {
+import com.assignment.nashtech.ecommerce.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategory();
+
+    Category getCategoryById(int categoryId);
+
+    Category saveCategory(Category category);
+
+    Category updateCategoryById(int categoryId, Category category);
+
+    void deleteCategoryById(int categoryId);
 }
